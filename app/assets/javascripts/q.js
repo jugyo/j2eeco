e2575a5754f7c1191992153271f8d85c69159244
jugyo <jugyo.org@gmail.com>
2013-02-18 12:28:59 -0800

    fix ui

--- app/assets/javascripts/q.js
+++ app/assets/javascripts/q.js
@@ -2,12 +2,14 @@ $(function() {
   $('#q_text').bind('textchange', function() {
     var length = $(this).val().length;
     var rest = 140 - length
-    if (rest > 0) {
+    if (rest >= 0) {
       $('#text_count').text(rest);
       $('#text_count').removeClass('over');
+      $('#new_q').find('input[type=submit]').removeAttr('disabled');
     } else {
       $('#text_count').text(rest);
       $('#text_count').addClass('over');
+      $('#new_q').find('input[type=submit]').attr('disabled','disabled');
     }
   })
 });

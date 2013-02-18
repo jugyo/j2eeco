cec8dbde162fb1567d4f5959578c755df57a97ee
jugyo <jugyo.org@gmail.com>
2013-02-18 00:48:30 -0800

    fix tweet text

--- app/workers/tweet_worker.rb
+++ app/workers/tweet_worker.rb
@@ -1,9 +1,11 @@
+# encoding: utf-8
 class TweetWorker
   include Sidekiq::Worker
 
   def perform(q_id)
     q    = Q.find(q_id)
-    text = q.text.truncate(140 - 23 - 3 - 2)
+    # NOTE: 'xxx... →en url'
+    text = q.text.truncate(140 - 23 - 3 - 2 - 4) + " →#{q.to}"
     url  = "http://#{Settings.url_options.host}/q/#{q.id}"
     twitter.update "#{text} #{url}" if Rails.env.production?
     logger.info "Tweet: " + "'#{text} #{url}'"
